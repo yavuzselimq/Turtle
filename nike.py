30b9108a9375a19db867183e7d3da0e481c8c6e5
Yavuz Selim <103683696+kratenist@users.noreply.github.com>
2022-04-17 01:39:25 +0300

    nike
    nike

---
+++ nike.py
@@ -0,0 +1,20 @@
+from turtle import*
+color("white")
+bgcolor("black")
+
+hideturtle()
+left(180)
+up()
+
+circle (700,10)
+down()
+
+begin_fill()
+circle(30,190)
+forward(200)
+left(173)
+forward(190)
+circle(-18,170)
+
+end_fill()
+done()
